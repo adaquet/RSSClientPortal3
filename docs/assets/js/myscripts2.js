8d9486028a6dff9b463e4b356fe12cff28780b8c
Anthony Daquet <AnthonyD@profretail.local>
2021-09-10 17:01:30 -0400

    Update 9/10/21 - Added js for hidding/showing tables. Need to figure out why pressing button in one section closes the other.

---
+++ docs/assets/js/myscripts2.js
@@ -0,0 +1,20 @@
+var divs = ["Section4", "Section5"];
+var visibleId = null;
+function show2(id) {
+if(visibleId !== id) {
+visibleId = id;
+} 
+hide2();
+}
+function hide2() {
+var div, i, id;
+for(i = 0; i < divs.length; i++) {
+id = divs[i];
+div = document.getElementById(id);
+if(visibleId === id) {
+div.style.display = "block";
+} else {
+div.style.display = "none";
+}
+}
+} 
