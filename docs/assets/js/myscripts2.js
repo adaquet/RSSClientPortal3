834a5ea01c3304be6045a041dd7c57c708b9e956
Adriana Monster <AdrianaMonster@profretail.local>
2021-09-14 16:56:27 -0400

    added white noise to login page, added the script to have both tables stay open, moved motivational quote to the left, added popovers and retun html content so that tables, div and dates can be returned for visit status more dates button

--- docs/assets/js/myscripts2.js
+++ docs/assets/js/myscripts2.js
@@ -1,17 +1,25 @@
-var divs = ["Section4", "Section5"];
-var visibleId = null;
+const cursor = document.querySelector('.cursor');
+window.addEventListener('mousemove', (e) =>{
+   cursor.style.left = e.pageX + 'px';
+   cursor.style.top = e.pageY + 'px';
+   consoler.log(e.pageX, e.pageY) 
+})
+
+
+var divs2 = ["Section4", "Section5"];
+var visibleId2 = null;
 function show2(id) {
-if(visibleId !== id) {
-visibleId = id;
+if(visibleId2 !== id) {
+visibleId2 = id;
 } 
 hide2();
 }
 function hide2() {
 var div, i, id;
-for(i = 0; i < divs.length; i++) {
-id = divs[i];
+for(i = 0; i < divs2.length; i++) {
+id = divs2[i];
 div = document.getElementById(id);
-if(visibleId === id) {
+if(visibleId2 === id) {
 div.style.display = "block";
 } else {
 div.style.display = "none";
